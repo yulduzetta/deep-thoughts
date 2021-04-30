f7aa54bee944fd1bb843649fa1795030bc5c8c8a
yulduz <yulduz83@gmail.com>
2021-04-30 02:40:40 -0500

    fixes  ReactionsForm component

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,33 +1,33 @@
-import { useState } from "react";
-import { useMutation } from "@apollo/react-hooks";
+import React, { useState } from 'react';
 
-import { ADD_REACTION } from "../../utils/mutations";
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_REACTION } from '../../utils/mutations';
 
 const ReactionForm = ({ thoughtId }) => {
-  const [reactionBody, setBody] = useState("");
-  const [charCount, setCharCount] = useState(0);
+  const [reactionBody, setBody] = useState('');
+  const [characterCount, setCharacterCount] = useState(0);
   const [addReaction, { error }] = useMutation(ADD_REACTION);
 
-  const handleChange = (event) => {
+  // update state based on form input changes
+  const handleChange = event => {
     if (event.target.value.length <= 280) {
       setBody(event.target.value);
-      setCharCount(event.target.value.length);
+      setCharacterCount(event.target.value.length);
     }
   };
 
-  // Submitting a reaction should automatically display the new reaction on the Single Thought page.
-  // Updating the cache works seamlessly, because the mutation returns the parent thought object that includes the updated reactions array as a property.
-  // If the mutation returned the reaction object instead, then we'd have another situation in which the cache would need a manual update.
-  const handleFormSubmit = async (event) => {
+  // submit form
+  const handleFormSubmit = async event => {
     event.preventDefault();
 
     try {
       await addReaction({
-        variables: { thoughtId, reactionBody },
+        variables: { reactionBody, thoughtId }
       });
-      // clean up
-      setBody("");
-      setCharCount(0);
+
+      // clear form value
+      setBody('');
+      setCharacterCount(0);
     } catch (e) {
       console.error(e);
     }
@@ -35,26 +35,27 @@ const ReactionForm = ({ thoughtId }) => {
 
   return (
     <div>
-      <p className="m-0">
-        Character Count: {charCount}/280
-        {error && <span className="m-2">Something went wrong...</span>}
+      <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
+        Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
       </p>
-      <form className="flex-row justify-center justify-space-between-md align-stretch">
+      <form
+        className="flex-row justify-center justify-space-between-md align-stretch"
+        onSubmit={handleFormSubmit}
+      >
         <textarea
-          onChange={handleChange}
           placeholder="Leave a reaction to this thought..."
+          value={reactionBody}
           className="form-input col-12 col-md-9"
+          onChange={handleChange}
         ></textarea>
 
-        <button
-          onClick={handleFormSubmit}
-          value={reactionBody}
-          className="btn col-12 col-md-3"
-          type="submit"
-        >
+        <button className="btn col-12 col-md-3" type="submit">
           Submit
         </button>
       </form>
+
+      {error && <div>Something went wrong...</div>}
     </div>
   );
 };
